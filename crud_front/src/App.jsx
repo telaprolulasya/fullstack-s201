b93654bec61ba5685809ff17a8c00f1f6407d86a
telaprolulasya <2300031910cseh@gmail.com>
2025-08-30 08:32:55 +0530

    Update App.jsx

--- crud_front/src/App.jsx
+++ crud_front/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   const handleChange = (e) => {
     setProduct({ ...product, [e.target.name]: e.target.value });
   };
-const BASE_URL = 'http://13.53.255.49:8080/springapp1';
+const BASE_URL = 'http://13.53.255.49:9090/springapp1';
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
